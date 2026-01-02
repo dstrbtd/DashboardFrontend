dea49c10a4892514e49dcf4b3b596273164cf419
JorritvanGils <vangilsjorrit@gmail.com>
2026-01-02 19:36:23 +0100

    move hardcoded ngroc link to cerntral urls file

--- src/components/MinerGraph.jsx
+++ src/components/MinerGraph.jsx
@@ -10,7 +10,7 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import websocketConfig from '../config/websocketUrls'; 
+import endpointConfig from '../config/endpointUrls'; 
 
 ChartJS.register(
   CategoryScale,
@@ -29,7 +29,7 @@ export default function MinerGraphV1() {
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const ws = new WebSocket(websocketConfig.WS_URL);
+    const ws = new WebSocket(endpointConfig.WS_URL);
 
     wsRef.current = ws;
 
